e32fb2f1b88089fb0eb3c1c88b70b053d2b2a4da
vladd12 <vladd12z32@gmail.com>
2019-05-05 17:32:19 +0300

    Add files via upload

---
+++ ProgramVL15/Scripts/SwitchScenes.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SwitchScenes : MonoBehaviour {
+	public void MySwitchScenes() {
+        SceneManager.LoadScene("scene1");
+    }
+}
